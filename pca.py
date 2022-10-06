43745741cc357e3f1721a1ba9a2ee99862698a27
entest <hai@entest.io>
2022-10-06 07:36:11 +0000

    add create model and batch transform pca

--- pca.py
+++ pca.py
@@ -10,12 +10,14 @@ from time import strftime
 import boto3
 import sagemaker
 from sagemaker import image_uris
+from sagemaker.amazon.amazon_estimator import get_image_uri
 from sagemaker.inputs import TrainingInput
 from sagemaker.processing import (
     ProcessingInput,
     Processor,
     ProcessingOutput,
 )
+from sagemaker.transformer import Transformer
 
 # environ variables and configuration
 if "SAGEMAKER_ROLE" in os.environ and "BUCKET_NAME" in os.environ:
@@ -75,7 +77,7 @@ def process_data():
         outputs=[
             ProcessingOutput(
                 source=f"{container_base_path}/output/train",
-                destination=f"{data_output_path}/train",
+                destination=f"{data_output_path}",
                 output_name="train",
             )
         ],
@@ -107,12 +109,68 @@ def train_pca_model():
         inputs={
             "train": TrainingInput(
                 content_type="text/csv;label_size=0",
-                s3_data=f"s3://{config['SAGEMAKER_BUCKET']}/ecg/test_small.csv",
+                s3_data=f"s3://{config['SAGEMAKER_BUCKET']}/pca-processed-data",
             )
         }
     )
     # return
     return None
+
+
+def get_trained_model():
+    """
+    get the trained model
+    """
+    sg = boto3.client('sagemaker')
+    training_jobs = sg.list_training_jobs()
+    # print(training_jobs["TrainingJobSummaries"][0])
+    # training job name
+    training_job_name = training_jobs["TrainingJobSummaries"][0]["TrainingJobName"]
+    training_job_des = sg.describe_training_job(
+        TrainingJobName=training_job_name)
+    print(training_job_des["ModelArtifacts"])
+    print(training_job_des)
+    return training_job_des["ModelArtifacts"]["S3ModelArtifacts"]
+
+
+def create_model(model_name):
+    """
+    create a model
+    """
+    # get image_uri
+    image_uri = image_uris.retrieve(
+        framework="pca",
+        region=os.environ["REGION"],
+        instance_type="t2.medium",
+    )
+    # get model artifact
+    model_artifact = get_trained_model()
+    # create a model
+    sg = boto3.client('sagemaker')
+    sg.create_model(ModelName=model_name,
+                    ExecutionRoleArn=os.environ['SAGEMAKER_ROLE'],
+                    PrimaryContainer={
+                        "Image": image_uri,
+                        "ModelDataUrl": model_artifact}
+                    )
+
+
+def batch_transform():
+    """
+    pca batch_transform
+    """
+    # transformer
+    transformer = Transformer(
+        model_name='my-pca-model',
+        instance_count=1,
+        instance_type='ml.c4.xlarge',
+        strategy="MultiRecord"
+    )
+    transformer.transform(
+        data=f's3://{os.environ["SAGEMAKER_BUCKET"]}/ecg/transform-input/',
+        content_type="text/csv",
+        split_type="Line"
+    )
 
 
 if __name__ == "__main__":
@@ -121,5 +179,8 @@ if __name__ == "__main__":
         os.environ["SAGEMAKER_BUCKET"]
     ).upload_file("./process_raw_data.py", "code/process_raw_data.py")
     # processing job
-    process_data()
+    # process_data()
     # train_pca_model()
+    # get_trained_model()
+    # create_model('my-pca-model')
+    batch_transform()
